0e8232580b31804d4ff680752bffa1942fc1a000
hsin-yi <hsinyic.uma@gmail.com>
2025-04-01 02:38:10 -1000

    cleanup

--- script/run_strategies.py
+++ script/run_strategies.py
@@ -10,7 +10,7 @@ import os
 
 # Get the absolute path to the data directory
 base_dir = os.path.dirname(os.path.abspath("__main__"))  # This will get the directory of your script
-data_dir = os.path.join(base_dir, '..', 'data')  # Going one level up and then to the 'data' folder
+data_dir = os.path.join(base_dir, 'data')  # Going one level up and then to the 'data' folder
 
 
 bidding_instance = BiddingOptimizer()
@@ -26,7 +26,7 @@ bidding_instance.set_wind_capacity_mw(windfarm_mw)
 data, _ = generate_sample_data(500, 24, 150)
 bidding_instance.set_data(data) # dalmp, rtlmp,windgen 
 bidding_instance.set_wind_capacity_mw(150) 
-'''
+
 
 # print("----- self schedule -------")
 bidding_instance.set_strategy(new_strat=SELF_SCHEDULE)
@@ -47,13 +47,14 @@ bidding_instance.set_strategy(new_strat=ECONOMIC_BID_P_ARGMAX)
 bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.8, 'beta':0}) 
 bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.95, 'beta':0.5}) 
 bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.95, 'beta':0.8})  
-'''
+
 
 print("----- economic bidding dual offer and bid: submit both offer and bid each with different price -------")
 bidding_instance.set_strategy(new_strat=ECONOMIC_BID_DUAL)
-bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.8, 'beta':0}) 
-bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.95, 'beta':0.5}) 
-bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.95, 'beta':0.8})  
+solver_option = ['scip', '/Users/hchen/miniforge3/envs/Tensorflow/bin/scip', True]
+bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.8, 'beta':0},solver_option=solver_option) 
+bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.95, 'beta':0.5}, solver_option=solver_option)
+bidding_instance.optimize(visualize_option=True, cvar={'alpha':0.95, 'beta':0.8}, solver_option=solver_option)
 revenues_spread = bidding_instance.get_revenue_dict() 
 plot_histogram(revenues_spread)
 plot_strategy_averages(revenues_spread)
